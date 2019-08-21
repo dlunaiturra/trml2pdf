9f0af94e95538e0746286b3daa308aafedd2693e
Luna David <david.luna@coneptum.com>
2019-08-21 11:49:14 +0200

    Implemented numbered canvas

--- trml2pdf/trml2pdf.py
+++ trml2pdf/trml2pdf.py
@@ -51,6 +51,29 @@ def _child_get(node, childs):
             clds.append(n)
     return clds
 
+class NumberedCanvas(canvas.Canvas):
+    def __init__(self, *args, **kwargs):
+        canvas.Canvas.__init__(self, *args, **kwargs)
+        self._saved_page_states = []
+
+    def showPage(self):
+        self._saved_page_states.append(dict(self.__dict__))
+        self._startPage()
+
+    def save(self):
+        """add page info to each page (page x of y)"""
+        num_pages = len(self._saved_page_states)
+        for state in self._saved_page_states:
+            self.__dict__.update(state)
+            self.draw_page_number(num_pages)
+            canvas.Canvas.showPage(self)
+        canvas.Canvas.save(self)
+
+    def draw_page_number(self, page_count):
+        self.setFont("Helvetica", 7)
+        self.drawRightString(200*mm, 20*mm,
+            "Page %d of %d" % (self._pageNumber, page_count))
+
 
 class _rml_styles(object):
 
@@ -244,7 +267,7 @@ class _rml_doc(object):
             pt_obj.render(
                 self.dom.documentElement.getElementsByTagName('story')[0])
         else:
-            self.canvas = canvas.Canvas(out)
+            self.canvas = NumberedCanvas(out)
             pd = self.dom.documentElement.getElementsByTagName(
                 'pageDrawing')[0]
             pd_obj = _rml_canvas(self.canvas, None, self)
